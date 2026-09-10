b9f0c7b3a6d5872d9c0d05757e4ddeafb1e44d07
godetremy <godetremy@free.fr>
2026-09-10 10:15:40 +0200

    fix(Icons): component size and icon name handling

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -1,10 +1,12 @@
 import { Icon as IconifyIcon, IconProps as IconifyIconProps } from "@iconify/react";
+import majesticons from "@iconify/json/json/majesticons.json";
 
 export interface IconProps extends Omit<IconifyIconProps, "icon"> {
     icon?: string;
-    name: string,
+    name: keyof typeof majesticons.icons,
+    size?: number
 }
 
-export default function Icon({icon, name, ...props}: IconProps) {
-    return <IconifyIcon icon={icon ?? `majesticons:${name}`} {...props} />
+export default function Icon({icon, name, size = 20, ...props}: IconProps) {
+    return <IconifyIcon icon={icon ?? `majesticons:${name}`} width={size} height={size} {...props} />
 }
