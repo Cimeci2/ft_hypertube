2f3db0362f9d19b7410afdbb3d17c21023ec0d74
godetremy <godetremy@free.fr>
2026-09-10 14:28:46 +0200

    feat(Header): update header and layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,39 @@
-export default function Header() {  
+import Logo from "@/components/ui/Logo";
+import Stack from "@/components/ui/Stack";
+import {Button} from "@/components/ui/Button";
+import Icon from "@/components/ui/Icon";
+import {useTranslations} from "next-intl";
+
+export default function Header() {
+    const t = useTranslations();
+
     return (
-        <>
-            <div className='bg-amber-50 flex justify-between p-4 rounded-lg text-[var(--text-color-2)] shadow-md gap-0.5'>
-                {/* left place */}
-                <div>
-                    logo
-                </div>
-                {/* title center place */}
-                <div className='font-semibold'>
-                    ScrollStream
-                </div>
-                {/* profil right place */}
-                <div className="gap-2">
-                    profile
-                </div>
-            </div>
-        </>
+        <Stack height={80} vAlign={"center"} padding={20} background={"gradientBackgroundBottom"} style={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 1000 }}>
+            <Stack
+                direction={"horizontal"}
+                width={"100%"}
+                maxWidth={1200}
+                vAlign={"center"}
+                gap={"auto"}
+            >
+                <Logo size={30} />
+
+                <Stack direction={"horizontal"} vAlign={"center"}>
+                    <Button
+                        variant={"borderless"}
+                        href={"/auth/register"}
+                    >
+                        {t("auth.register.title")}
+                    </Button>
+                    <Button
+                        variant={"primary"}
+                        left={ <Icon name={"arrow-right"} /> }
+                        href={"/auth/login"}
+                    >
+                        {t("auth.login.title")}
+                    </Button>
+                </Stack>
+            </Stack>
+        </Stack>
     )
 }
