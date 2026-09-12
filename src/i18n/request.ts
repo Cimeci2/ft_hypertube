fe938d6fadd813f6a5192a2d6494379af7295146
godetremy <godetremy@free.fr>
2026-09-12 17:55:14 +0200

    fix: remove console logs

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -13,8 +13,6 @@ async function getLocaleFromHeaders() {
         .split(',')
         .map((part) => part.split(';')[0].trim().split('-')[0]);
 
-    console.log(preferred)
-
     return preferred.find((lang) => locales.includes(lang)) ?? defaultLocale;
 }
 
@@ -22,8 +20,6 @@ export default getRequestConfig(async () => {
     const store = await cookies();
     const locale = store.get('app-lang')?.value || (await getLocaleFromHeaders());
 
-    console.log(locale)
-    
     return {
         locale,
         messages: (await import(`./lang/${locale}.json`)).default
